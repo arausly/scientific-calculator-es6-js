3fb32977f223217579d96d6400f428b9e132046f
Arausi Daniel <arausid@yahoo.com>
2017-06-18 13:54:47 +0100

    intial commit

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,17 +1,16 @@
 let btns,btn,store = [0],operandA =[],operandB = [],operator = [];
 
 btns = Array.prototype.slice.call(document.querySelectorAll('.btn'));  // this has various implementations for different browsers.
-btn = btns.map((button)=>{
+
+
+const btnLogic = (button) =>{
 	let btnVal = button.getAttribute('value');
 	button.addEventListener('click',(e)=>{
-		if(!e){
-			e === window.event;
-		}
+		e = e === undefined ? window.event : e;
 		e.preventDefault();
+		store.push()
 		if(btnVal === null || btnVal === undefined){
-			//			   console.log(button.innerHTML);
-
-
+			//console.log(button.innerHTML);
 		}else{
 			//			  console.log(button.getAttribute('value'));
 			let numBtnVal = Number(btnVal);
@@ -19,13 +18,35 @@ btn = btns.map((button)=>{
 				operandA.push(numBtnVal);
 
 				console.log(operandA.length);
+				console.log(operandA);
 			}else if(!Number.isFinite(numBtnVal)){
 				operator.push(btnVal);
 				console.log(operator.length);
+				if(operator.length === 1){
+					let operandAInit = operandA.length;
+					if(operandAInit){
+						switch(operator[0]){
+							case 'Del':
+								operandA.pop(operandAInit -1);
+								console.log(operandA);
+								break;
+							default:
+								console.log('Some default');
+								break;	
+						}
+					}else if(!operandAInit){
+						operandA.splice(operandAInit - 1);
+						operandB.push(btnVal);
+						console.log(operandB.length);
+					}
+				}
 			}
 		}
 	}); 
-	});
+}
 
+// const block						
+
+btn = btns.map((btn)=>{btnLogic(btn)});
 
 document.getElementById('result').innerHTML = store[0];
